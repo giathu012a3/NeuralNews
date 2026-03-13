4c3c032e9456c90e66707935fcac81ee0c724585
Nguyen Za Thu <nguyengiathu24052004@gmail.com>
2026-03-13 19:08:15 +0700

    Force-sync Test code to match article_desgin

--- src/main/java/neuralnews/model/Category_Model.java
+++ src/main/java/neuralnews/model/Category_Model.java
@@ -4,27 +4,16 @@ public class Category_Model {
     private int id;
     private String name;
 
-    public Category_Model() {
-    }
+    public Category_Model() {}
 
     public Category_Model(int id, String name) {
         this.id = id;
         this.name = name;
     }
 
-    public int getId() {
-        return id;
-    }
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 }
